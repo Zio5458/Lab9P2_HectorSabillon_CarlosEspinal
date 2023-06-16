4fa944fb72cff30d56f36e54eed179bd7826ebd1
Hector Sabillon <hdannysabillon992232@gmail.com>
2023-06-16 16:37:49 -0600

    base de pijasos

--- src/Clase.java
+++ src/Clase.java
@@ -4,11 +4,10 @@ import java.util.ArrayList;
 
 public class Clase extends Asignatura {
     
-    private double precio;
+    private double precio = 0.20;
 
     public Clase(double precio, String nombre, String cod_asignatura, String cod_seccion, String horario, Maestro docente, int uv, int aula, int edificio, ArrayList<Alumnos> alumnos, String modalidad, String dias) {
         super(nombre, cod_asignatura, cod_seccion, horario, docente, uv, aula, edificio, alumnos, modalidad, dias);
-        this.precio = precio;
     }
 
     public double getPrecio() {
